a52728d3a4637cd9ed44ca906e4283dbef27af81
Richard Ben <frichardben@yahoo.com.br>
2021-04-20 07:49:34 -0300

    style: button colors

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -44,6 +44,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                         type="button"
                         onClick={() => {setType('deposit'); }}
                         isActive={type === 'deposit'}
+                        activeColor="green"
                     >
                         <img src={incomeImg} alt="Entrada" />
                         <span>Entrada</span>
@@ -54,6 +55,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                         type="button"
                         onClick={() => {setType('withdraw'); }}
                         isActive={type === 'withdraw'}
+                        activeColor="red"
                     >
                         <img src={outcomeImge} alt="Saída" />
                         <span>Saída</span>
