2735a80fcb9fa4cf2809144a5c8b4fe7ba0e7d3e
Richard Ben <frichardben@yahoo.com.br>
2021-04-20 10:31:54 -0300

    feat: listing of transactions and seeds

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-04-20 09:00:00'),
+        }, 
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Ap',
+          amount: 900,
+          createdAt: new Date('2021-04-10 08:00:00'),
+        }
+      ],
+    })
+  },
+
   routes() {
     this.namespace = 'api'
 
