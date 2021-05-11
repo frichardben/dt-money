429880ca140daa1510c9be1d63e42e2571e97457
Richard Ben <frichardben@yahoo.com.br>
2021-05-11 10:39:26 -0300

    refactor: created hooks

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useEffect, useState } from 'react'; 
-import { api } from './services/api';
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react'; 
+import { api } from '../services/api';
 
 
 interface Transaction {
@@ -22,7 +22,7 @@ interface TransactionsContextData {
     createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
     {} as TransactionsContextData
 );
 
@@ -52,4 +52,10 @@ export function TransactionsProvider({ children }: TransactionsProviderPros) {
             {children}
         </TransactionsContext.Provider>
     )
+}
+
+export function useTransactions() {
+    const context = useContext(TransactionsContext);
+
+    return context;
 }
