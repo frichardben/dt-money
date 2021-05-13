5ef05542caf2a3253561f757d6612a45f832f7bf
Richard Ben <frichardben@yahoo.com.br>
2021-05-13 12:10:47 -0300

    fix: url correction in production

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const locationURL = window.location.origin
 export const api = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    //baseURL: 'http://localhost:3000/api'
+    baseURL: `${locationURL}/api`
 })
